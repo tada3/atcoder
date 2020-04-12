52e2d04b54a4c52339d0332f7565451c2b5a96e1
tada <tada3@outlook.com>
2020-04-12 17:42:25 +0900

    getting slower..

--- abc129/d_2/src/main.rs
+++ abc129/d_2/src/main.rs
@@ -23,32 +23,55 @@ fn main() {
     let h = hw[0];
     let w = hw[1];
 
-    let mut shogai_yoko : Vec<Vec<usize>> = Vec::with_capacity(h);
-    let mut shogai_tate : Vec<Vec<usize>> = Vec::with_capacity(w);
+    let mut shogai_yoko : Vec<Vec<(usize, usize)>> = Vec::with_capacity(h);
+    let mut shogai_tate : Vec<Vec<(usize, usize)>> = Vec::with_capacity(w);
     for _i in 0..w {
-        let v : Vec<usize> = Vec::with_capacity(h);
-        shogai_tate.push(v)
+        let u : Vec<(usize, usize)> = Vec::with_capacity(h);
+        shogai_tate.push(u);
     }
     
     for i in 0..h {
-        let mut v : Vec<usize> = Vec::with_capacity(w);
+        let mut v : Vec<(usize, usize)> = Vec::with_capacity(w);
         let s = read::<String>();
+        let mut in_shogai_yoko = false;
+        let mut in_shogai_tate = vec![false;w];
         for (j, c) in s.chars().enumerate() {
             if c == '#' {
-                v.push(j);
-                shogai_tate[j].push(i)
-            }
+                if in_shogai_yoko {
+                    let last = v.last_mut().unwrap();
+                    last.1 = last.1 + 1;
+                } else {
+                    v.push((j, j+1));
+                    in_shogai_yoko = true;
+                }
+
+                let u = shogai_tate.get_mut(j).unwrap();
+                if in_shogai_tate[j] {
+                    let last = u.last_mut().unwrap();
+                    last.1 = last.1 + 1;
+                } else {
+                    u.push((i, i+1));
+                    in_shogai_tate[j] = true;
+                }
+            } else {
+                if in_shogai_yoko {
+                    in_shogai_yoko = false;
+                } 
+                if in_shogai_tate[j] {
+                    in_shogai_tate[j] = false;
+                }
+            } 
         }
        
-        if v.is_empty() || v[v.len()-1] < w - 1 {
-            v.push(w);
+        if v.is_empty() || v[v.len()-1].1 < w {
+            v.push((w, 0));
         }
         shogai_yoko.push(v);
     }
     for j in 0..w {
-        let v = &mut shogai_tate[j];
-        if v.is_empty() || v[v.len() - 1] < h - 1 {
-            v.push(h)
+        let u = &mut shogai_tate[j];
+        if u.is_empty() || u[u.len() - 1].1 < h {
+            u.push((h, 0))
         }
     }
 
@@ -57,7 +80,7 @@ fn main() {
     println!("{}", answer);
 }
 
-fn solve(h: usize, w: usize, shogai_yoko: &[Vec<usize>], shogai_tate: &[Vec<usize>]) -> usize {
+fn solve(h: usize, w: usize, shogai_yoko: &[Vec<(usize, usize)>], shogai_tate: &[Vec<(usize, usize)>]) -> usize {
      // (yoko, tate)
      let mut dp : Vec<Vec<usize>> = Vec::with_capacity(h);
      for _i in 0..h {
@@ -69,13 +92,13 @@ fn solve(h: usize, w: usize, shogai_yoko: &[Vec<usize>], shogai_tate: &[Vec<usiz
      for i in 0..h {
         let mut from = 0;
         for x in &shogai_yoko[i] {
-            let d = x - from;
+            let d = x.0 - from;
             if d > 1 {
-                for j in from..*x {
+                for j in from..x.0 {
                     dp[i][j] = d - 1;
                 }
             }
-            from = x + 1;
+            from = x.1
         }
     }
 
@@ -85,19 +108,24 @@ fn solve(h: usize, w: usize, shogai_yoko: &[Vec<usize>], shogai_tate: &[Vec<usiz
     for j in 0..w {
         let mut from = 0;
         for x in &shogai_tate[j] {
-            let d = x - from;
+            let d = x.0 - from;
             if d > 0 {
-                for i in from..*x {
-                    let val = dp[i][j] + (d - 1);
-                    if val > max {
-                        max = val;
-                        if max == limit {
-                            return max + 1;
-                        }
+                let mut yoko_max = 0;
+                for i in from..x.0 {
+                    let yoko = dp[i][j];
+                    if yoko > yoko_max {
+                        yoko_max = yoko;
+                    }
+                }
+                let val = yoko_max + (d - 1);
+                if val > max {
+                    max = val;
+                    if max == limit {
+                        return max + 1;
                     }
                 }
             }
-            from = x + 1;
+            from = x.1;
         }
     }
     return max + 1
